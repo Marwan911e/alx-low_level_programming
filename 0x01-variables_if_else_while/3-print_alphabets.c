8892173bcb871b1c68148a67a225a5951eb70916
marwan911e <marwan.04e@gmail.com>
2023-07-14 16:39:20 -0700

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,40 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Discription: A C program that prints the number is positive or negative
- *
- * Return: Always 0 (Success)
-*/
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Discription: A C program that prints the number is positive or negative
- *
- * Return: Always 0 (Success)
-*/
 int main(void)
 {
-	char ch = 'a';
-	char ch2 = 'A';
+	int i, y;
 
-	while (ch <= 'z')
-	{
-	putchar(ch);
-	ch++;
-	}
-	while (ch2 <= 'Z')
-	{
-	putchar(ch2);
-	ch2++;
-	}
-	putchar('\n');
-	return (0);
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	printf("\n");
+	for (y = 65; y < 91; y++)
+		putchar(y);
 }
