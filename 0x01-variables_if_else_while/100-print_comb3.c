74fedced460ed84c356d05256fc04d41f8a05bf9
marwan911e <marwan.04e@gmail.com>
2023-07-15 04:43:51 +0300

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j, z;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	return (0);
+}
