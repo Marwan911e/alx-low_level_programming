2eef4f881165a25023780d205f630d416cd1c253
marwan911e <marwan.04e@gmail.com>
2023-07-14 17:06:00 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+	if (i == 101)
+		continue;
+	if else(i == 113)
+		continue;
+	else
+		putchar(i);
+	}
+	return (0);
+}
