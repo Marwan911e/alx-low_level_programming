b072e249ea949c6b9ef556afe587a1487492dfab
marwan911e <marwan.04e@gmail.com>
2023-07-14 17:12:24 -0700

    done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int i;
 
 	for (i = 97; i < 123; i++)
 	{
-	if (i == 101)
-		continue;
-	else if (i == 113)
+	if (i == 101 || i == 113)
 		continue;
 	else
 		putchar(i);
