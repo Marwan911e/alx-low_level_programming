7970e8be791e376b67e484ba9ec3b095d9f26e10
marwan911e <marwan.04e@gmail.com>
2023-07-15 04:37:10 +0300

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int y = 48;
+
+	while (y < 58)
+	{
+	putchar(x);
+	if (y != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	y++;
+	}
+	putchar('\n');
+	return (0);
+}
