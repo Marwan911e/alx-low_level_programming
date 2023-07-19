633b7df8e88bcd8c9816159ba8e7d7e544b8d886
marwan911e <marwan.04e@gmail.com>
2023-07-19 03:27:51 +0300

    done

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that print the last digit of a positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that print the last digit of a positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int _putchar(char c)
+{
+
+	return (write(1, &c, 1));
+}
