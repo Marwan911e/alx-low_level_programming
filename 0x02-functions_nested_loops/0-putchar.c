ba526db527d06dd096b2094eb178b1e8cdd57293
marwan911e <marwan.04e@gmail.com>
2023-07-19 03:22:30 +0300

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,20 @@
+#include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that print the last digit of a positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	_putchar(str[i]);
+	_putchar('\n');
 	return (0);
 }
